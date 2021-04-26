fd1973ea03724d9b3f39e083e8245c1f8522b328
Beiqi Li <beiqi.li@mail.utoronto.ca>
2021-04-26 09:53:47 +0800

    code added

--- spider_multip.py
+++ spider_multip.py
@@ -2,6 +2,7 @@ import requests
 import re
 import os
 import time
+from get_image import get_image
 from multiprocessing import Process,JoinableQueue,cpu_count
 
 PROC_COUNT = 12
@@ -9,26 +10,6 @@ PROC_COUNT = 12
 heads = {
     'user-agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
 }
-
-def get_image(url,title,verbose=False):
-    filename = url.split('/')[-1]
-    path = title+filename
-    # print(title)
-    try:
-        if not os.path.exists(title):
-            os.mkdir(title)
-        if not os.path.exists(path):
-            r = requests.get(url)
-            with open (path, 'wb') as f:
-                f.write(r.content)
-                f.close()
-                if verbose:
-                    print("文件保存成功",filename)
-        else:
-            if verbose:
-                print("文件已存在",filename)
-    except:
-        print("爬取失败",filename)
 
 def crawl_webpage_recursive(url):
     html = ""
@@ -53,58 +34,46 @@ def crawl_webpage_recursive(url):
     enb_index = re.search(r"<img alt=.*?>",html).start()
     url = re.findall("<a href=.*?>", html[:enb_index])[-1][9:-2]
 
-    return url
+    match_list = re.findall(r"<h1.*?/h1>",html)
 
-
-def producer(q, url):
-    html=""
-    try:
-        response = requests.get(url,headers=heads)
-        html = response.text
-    except:
-        print("爬取失败")
-
-    match_list = re.findall(r"<h1>.*?</h1>",html)
-
-    title_str=match_list[0][4:-5]
-
-    match_list = re.findall(r"<img id=\"img\" src=.*?>",html)
-    img_url = re.split(r" ", match_list[0])[2][5:-1]
-    # print(img_url)
+    title_str=match_list[0][11:-5]
 
     title_str=re.sub(r'[/\\\"|<>?\*]','',title_str)
 
     print(title_str)
+    # quit()
 
     if title_str != "":
         title_str = title_str+"//"
 
-    match_list = re.findall(r"<a id=\"next\" onclick=\"return load_image.*?>",html)
-    next_url = re.split(r"href=",match_list[0])[-1][1:-2]
-    q.put((img_url,title_str))
-    while next_url != url:
-        url = next_url
+    return url,title_str
+
+
+def producer(q, url):
+    prev_url=""
+    while prev_url != url:
         try:
             response = requests.get(url,headers=heads)
-            html = response.text
         except:
             print("爬取失败")
 
-        match_list = re.findall(r"<img id=\"img\" src=.*?>",html)
+        match_list = re.findall(r"<img id=\"img\" src=.*?>",response.text)
         img_url = re.split(r" ", match_list[0])[2][5:-1]
         # print(img_url)
 
-        q.put((img_url,title_str))
+        q.put(img_url)
 
-        match_list = re.findall(r"<a id=\"next\" onclick=\"return load_image.*?>",html)
-        next_url = re.split(r"href=",match_list[0])[-1][1:-2]
+        match_list = re.findall(r"<a id=\"next\" onclick=\"return load_image.*?>",response.text)
+        prev_url = url
+        url = re.split(r"href=",match_list[0])[-1][1:-2]
+
 
     q.join()
 
-def consumer(q):
+def consumer(q,title):
     while True:
-        img_url,title_str=q.get()
-        get_image(img_url,title_str,verbose=True)
+        img_url=q.get()
+        get_image(img_url,title,verbose=True)
         q.task_done()#发送信号给生产者的q.join()说，已经处理完从队列中拿走的一个项目
 
 
@@ -122,7 +91,7 @@ if __name__ == '__main__':
     i = 0
     for url in urls:
         i=i+1
-        url=crawl_webpage_recursive(url)
+        url,title_str=crawl_webpage_recursive(url)
         print("Crawling {0}".format(url))
 
         q = JoinableQueue()
@@ -130,7 +99,7 @@ if __name__ == '__main__':
 
         consumer_list=[]
         for i in range(PROC_COUNT):
-            c=Process(target=consumer,args=(q,))
+            c=Process(target=consumer,args=(q,title_str))
             c.daemon = True
             consumer_list.append(c)
 
